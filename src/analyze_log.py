a73497ffee1e2055422c0bf1d13ac93de266db69
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-05 14:03:43 -0300

    first commit

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,24 @@
+# Qual o prato mais pedido por 'maria'?
+
+# Quantas vezes 'arnaldo' pediu 'hamburguer'?
+
+# Quais pratos 'joao' nunca pediu?
+
+# Quais dias 'joao' nunca foi na lanchonete?
+
+# hamburguer
+# 1
+# {'pizza', 'coxinha', 'misto-quente'}
+# {'sabado', 'segunda-feira'}
+
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file, "r") as file:
+        while file.read() != None:
+            pass
+        # print(array)
+
+
+analyze_log("data/orders_1.csv")
