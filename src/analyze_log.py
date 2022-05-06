0859769bd6432c58b657bbf69b48922bb6bf8b86
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-06 13:25:34 -0300

    adiciona as entradas em um dicionario

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -16,9 +16,12 @@ import csv
 
 def analyze_log(path_to_file):
     with open(path_to_file, "r") as file:
-        while file.read() != None:
-            pass
-        # print(array)
+        fieldnames = ['nome', 'comida', 'dia']
+        reader = csv.DictReader(file, fieldnames=fieldnames)
+        array = []
+        for row in reader:
+            array.append(row)
+        print(array)
 
 
 analyze_log("data/orders_1.csv")
